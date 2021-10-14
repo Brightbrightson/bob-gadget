41bc0e26f964d72980bf9c9302cee7f91838683f
your <brightbrightson9@gmail.com>
2021-10-14 19:48:44 +0100

    update our menu-item to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import {Switch, Route} from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+
+const ComputerAccessories = () => (
+  <div>
+    <h1>Computer & Accessories</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route
+          exact
+          path="/ComputerAccessories"
+          component={ComputerAccessories}
+        />
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -15,18 +15,21 @@ class Directory extends React.Component {
           imageUrl:
             "https://i.ibb.co/K0Lp4T0/onur-binay-4-Ykxp-t4i08-unsplash.jpg",
           id: 1,
+          linkUrl: "ComputerAccessories",
         },
         {
           title: "Electronics",
           imageUrl:
             "https://i.ibb.co/mRVCXMC/altumcode-Ui3z-Mjp-Mrm-M-unsplash.jpg",
           id: 2,
+          linkUrl: "",
         },
         {
           title: "Gaming accessories",
           imageUrl:
             "https://i.ibb.co/FmDSZGJ/florian-olivo-Mf23-RF8x-Ar-Y-unsplash.jpg",
           id: 3,
+          linkUrl: "",
         },
         {
           title: "VR",
@@ -34,6 +37,7 @@ class Directory extends React.Component {
             "https://i.ibb.co/qJZQ3xg/vinicius-amnx-amano-Mv-Jezf8-FT4o-unsplash.jpg",
           size: "large",
           id: 4,
+          linkUrl: "",
         },
         {
           title: "Kitchen Gadget",
@@ -41,6 +45,7 @@ class Directory extends React.Component {
             "https://i.ibb.co/wrvxmT7/jason-briscoe-Glia-HAJ3-5-A-unsplash.jpg",
           size: "large",
           id: 5,
+          linkUrl: "",
         },
       ],
     };
@@ -49,8 +54,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({title, imageUrl, id, size}) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({id, ...otherSectionProps}) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
